aff0ed3a6d2fe20f9c5bc686213ab94f8bb450a7
fivetran-reneeli <renee.li@fivetran.com>
2022-09-16 17:59:48 -0400

    add config,  unique key, date day, fix pivot cols

--- models/amplitude__event_enhanced.sql
+++ models/amplitude__event_enhanced.sql
@@ -1,13 +1,14 @@
 {{
     config(
         materialized='incremental',
-        unique_key='unique_key',
+        unique_key='unique_event_id',
         partition_by={
-            "field": "date_day",
+            "field": "event_day",
             "data_type": "date"
         }
     )
 }}
+
 with event_data as (
 
     select * 
@@ -21,13 +22,11 @@ event_type as (
 )
 
 select
+    {{ dbt_utils.surrogate_key(['ed.event_id','ed.session_id']) }} as unique_event_id,
     ed.event_id,
     ed.event_type,
-
-    {% if var('event_properties_to_pivot') %},
-    {{ fivetran_utils.pivot_json_extract(string = 'event_properties', list_of_properties = var('event_properties_to_pivot')) }}
-    {% endif %}
-
+    ed.event_time,
+    {{ dbt_utils.date_trunc('day', 'event_time') }} as event_day,
     ed.session_id,
     ed.amplitude_id,
     ed.app,
@@ -61,22 +60,24 @@ select
     ed.user_creation_time,
     row_number() over (partition by session_id order by event_time asc, event_id asc) as session_event_number,
     ed.group_types,
+    cast(ed.user_id as {{ dbt_utils.type_string() }}) as user_id, 
+    et.event_type_id,
+    et.event_type_name,
+    et.in_waitroom,
+    et.totals,
+    et.value
+
+    {% if var('event_properties_to_pivot') %},
+    {{ fivetran_utils.pivot_json_extract(string = 'event_properties', list_of_properties = var('event_properties_to_pivot')) }}
+    {% endif %}
 
     {% if var('group_properties_to_pivot') %},
     {{ fivetran_utils.pivot_json_extract(string = 'group_properties', list_of_properties = var('group_properties_to_pivot')) }}
     {% endif %}
 
-    ed.user_id,
-
     {% if var('user_properties_to_pivot') %},
     {{ fivetran_utils.pivot_json_extract(string = 'user_properties', list_of_properties = var('user_properties_to_pivot')) }}
     {% endif %}
-
-    et.event_type_id,
-    et.event_type_name,
-    et.in_waitroom,
-    et.totals,
-    et.value,
 
     from event_data ed 
     left join event_type et
