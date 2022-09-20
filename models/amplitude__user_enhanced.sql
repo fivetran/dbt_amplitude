f116cc3213e6aa4c0a9e3c541a63ab95d3e6aa99
fivetran-reneeli <renee.li@fivetran.com>
2022-09-20 15:18:33 -0400

    update join to unique session id

--- models/amplitude__user_enhanced.sql
+++ models/amplitude__user_enhanced.sql
@@ -16,13 +16,13 @@ session_data as (
 select
     ee.user_id,
     count(distinct ee.unique_event_id) as total_events_per_user,
-    count(distinct sd.session_id) as total_sessions_per_user,
+    count(distinct sd.unique_session_id) as total_sessions_per_user,
     avg(sd.session_length) as average_session_length,
     avg(sd.time_in_between_sessions) as average_time_in_between_sessions
 
 from event_enhanced ee
 left join session_data sd
-    on ee.session_id = sd.session_id
+    on ee.unique_session_id = sd.unique_session_id
 where ee.user_id is not null
 group by ee.user_id
 
