4d9d5a327394f7db9a77d9630765e9b21f5dfb2e
fivetran-reneeli <renee.li@fivetran.com>
2023-02-02 15:01:24 -0500

    oops the date field were right the first time

--- models/intermediate/int_amplitude__date_spine.sql
+++ models/intermediate/int_amplitude__date_spine.sql
@@ -42,7 +42,7 @@ spine as (
 
     {% if is_incremental() %} 
     
-    where event_day >= ( select max(event_day) from {{ this }} )
+    where date_day > ( select max(date_day) from {{ this }} )
     
     {% endif %}
 ),
@@ -52,11 +52,11 @@ date_spine as (
 
     select
         distinct event_data.event_type,
-        cast(spine.event_day as date) as event_day,
-        {{ dbt_utils.generate_surrogate_key(['spine.event_day','event_data.event_type']) }} as date_spine_unique_key
+        cast(spine.date_day as date) as event_day,
+        {{ dbt_utils.generate_surrogate_key(['spine.date_day','event_data.event_type']) }} as date_spine_unique_key
     from spine 
     join event_data
-        on spine.event_day >= event_data.event_day -- each event_type will have a record for every day since their first day
+        on spine.date_day >= event_data.event_day -- each event_type will have a record for every day since their first day
 )
 
 select * 
