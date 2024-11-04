ab849e5e1c56a7b9a0d4187fba7b803e458847c6
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-11-04 15:18:54 -0600

    update date_spine

--- models/intermediate/int_amplitude__date_spine.sql
+++ models/intermediate/int_amplitude__date_spine.sql
@@ -1,23 +1,6 @@
--- depends_on: {{ ref('amplitude__event_enhanced') }}
 {{ config(materialized='ephemeral') }}
 
 with spine as (
-    {% if execute and flags.WHICH in ('run', 'build') %}
-    {%- set first_date_query %}
-        select 
-            coalesce(
-                min(cast(event_day as date)), 
-                cast({{ dbt.dateadd("month", -1, "current_date") }} as date)
-                ) as min_date
-            from {{ ref('amplitude__event_enhanced') }} 
-    {% endset -%}
-    {%- set first_date = dbt_utils.get_single_value(first_date_query) %}
-    
-    {% else %} 
-    {% set first_date = "2020-01-01" %}
-
-    {% endif %}
-
     {%- set last_date_query %}
         select 
             cast({{ dbt.dateadd("day", 1, dbt.current_timestamp()) }} as date) as max_date
@@ -30,7 +13,7 @@ with spine as (
     from (
         {{ dbt_utils.date_spine(
             datepart = "day", 
-            start_date =  "cast('" ~ var('amplitude__date_range_start',  first_date) ~ "' as date)", 
+            start_date =  "cast('" ~ var('amplitude__date_range_start',  '2020-01-01') ~ "' as date)"
             end_date = "cast('" ~ var('amplitude__date_range_end', last_date) ~ "' as date)" 
             )
         }} 
