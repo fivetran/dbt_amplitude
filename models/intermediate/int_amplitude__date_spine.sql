66e12ac93ae8b792e1538bee4f51482b86b4abda
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-11-04 15:28:58 -0600

    update date_spine

--- models/intermediate/int_amplitude__date_spine.sql
+++ models/intermediate/int_amplitude__date_spine.sql
@@ -1,12 +1,12 @@
 {{ config(materialized='ephemeral') }}
 
 with spine as (
-    {%- set end_date_query %}
-        select 
-            cast({{ dbt.dateadd("day", 1, dbt.current_timestamp()) }} as date) as max_date
-    {% endset -%}
+{% set end_date_query %}
+    -- select one day past current day
+    select cast({{ dbt.dateadd("day", 1, dbt.current_timestamp()) }} as date) as max_date
+{% endset %}
 
-    {%- set last_date = dbt_utils.get_single_value(end_date_query) %}
+{%- set end_date_adjust = dbt_utils.get_single_value(end_date_query) %}
 
     select
         cast(spine.date_day as date) as date_day
@@ -14,7 +14,7 @@ with spine as (
         {{ dbt_utils.date_spine(
             datepart = "day", 
             start_date =  "cast('" ~ var('amplitude__date_range_start',  '2020-01-01') ~ "' as date)",
-            end_date = "cast('" ~ var('amplitude__date_range_end', last_date) ~ "' as date)" 
+            end_date = "cast('" ~ var('amplitude__date_range_end', end_date_adjust) ~ "' as date)" 
             )
         }} 
     ) as spine
